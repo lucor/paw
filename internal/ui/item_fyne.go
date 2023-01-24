c6757b0b69bbf02b58af43025ae8265fa165c160
lucor <lu.corbo@gmail.com>
2023-01-24 19:24:13 +0100

    ui: add password revealer widget
    This commit adds the password revelear widget.
    This will allow to show the stored password as  a label instead of a disabled text entry improving the readability and UX.

--- internal/ui/item_fyne.go
+++ internal/ui/item_fyne.go
@@ -16,6 +16,7 @@ import (
 
 	"lucor.dev/paw/internal/icon"
 	"lucor.dev/paw/internal/paw"
+	pawwidget "lucor.dev/paw/internal/widget"
 )
 
 // FyneItem wraps all methods allow to handle an Item as Fyne canvas object
@@ -117,11 +118,7 @@ func rowWithAction(label string, text string, opts rowActionOptions, w fyne.Wind
 	var v fyne.CanvasObject
 	switch opts.widgetType {
 	case "password":
-		t := widget.NewPasswordEntry()
-		t.SetText(text)
-		t.Disable()
-		t.Validator = nil
-		v = t
+		v = pawwidget.NewPasswordRevealer(text)
 	case "url":
 		u, err := url.Parse(text)
 		if err == nil && strings.HasPrefix(u.Scheme, "http") {
